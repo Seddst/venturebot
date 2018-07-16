093c5dd13a7998b81344506dd77e5fc3879573ee
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-16 14:42:27 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = '618668131:AAG57FawPhzS5FEBsyGRsSZ1aIxbh871Ei0'  # token telegram (is obtained from @BotFather)
-DB = 'mysql+pymysql://ndceo8242qg0i5md:gt5jw4zmvw5ojpcp@o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gu6d4411p51bbdfk'
+DB = 'mysql://v79s2g6ae3v1qb8u:xltsnwijdjm61zdr@g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fkct7nz1f2qcbrbk'
 
 API_PORT = 3306
 
