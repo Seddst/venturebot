2c2dce03a40b7db572e86feef6dd0061c5ff37b3
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-16 14:14:03 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '618668131:AAG57FawPhzS5FEBsyGRsSZ1aIxbh871Ei0'  # token telegram (is obtained from @BotFather)
+DB = 'mysql+pymysql://ndceo8242qg0i5md:gt5jw4zmvw5ojpcp@o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gu6d4411p51bbdfk'
+
+API_PORT = 3306
+
+VENTURE_CHAT_ID = 618668131
