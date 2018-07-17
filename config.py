99ee991595e0eba9c6d3e1cc227cc263c5b45390
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-17 12:49:44 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = '618668131:AAG57FawPhzS5FEBsyGRsSZ1aIxbh871Ei0'  # token telegram (is obtained from @BotFather)
-DB = 'mysql://v79s2g6ae3v1qb8u:xltsnwijdjm61zdr@g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fkct7nz1f2qcbrbk'
+DB = 'postgres://joudxzumtwobde:1eeb6745d937cde8d6ab64be2680970b73fc296089996bfaaf8474666ce34011@ec2-107-21-126-193.compute-1.amazonaws.com:5432/df71rnpnhaktpc'
 
 API_PORT = 3306
 
