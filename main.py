e98ad7a2d3b4be4ce9de194516774fbd07e704ac
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-18 11:34:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def help_msg(self, update):
 
 
 def ping(self, bot: Bot):
-    send_async(self, bot, update, chat_id=update.message.chat.id,
+    send_async(self, bot, chat_id=update.message.chat.id,
                text=message('Go and dig some soulz, @{}!'))
 
 
