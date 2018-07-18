7198b2ad7944a2689b3b9b8cf72b0566d93f246a
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-18 12:43:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ def del_trigger(bot, update, session):
         send_async(bot, chat_id=update.message.chat.id, text='Where did you see such a trigger? 0_o')
 
 
-def list_triggers(update: Update, session):
+def list_triggers(update: Update, session, bot: Bot):
     triggers = session.query(Trigger).all()
     local_triggers = session.query(LocalTrigger).filter_by(chat_id=update.message.chat.id).all()
     msg = 'List of current triggers: \n' + \
