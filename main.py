b2c6aa9e8f10cfe7c61fa4c8950f21061c8e2cc1
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-18 10:29:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -77,8 +77,8 @@ def help_msg(self, update):
     update.message.reply_text("/list_triggers — show all triggers.")
 
 
-def ping(self, update):
-    send_async(chat_id=update.message.chat.id,
+def ping(self, bot, update):
+    send_async(self, bot, chat_id=update.message.chat.id,
                text='Go and dig some soulz, @{}!'.format(update.message.from_user.username))
 
 
