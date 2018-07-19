fc46ad1aacfa4b04569d9a42b1cd114e02935665
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-18 22:38:28 -0700

    Update typing.py

--- typing.py
+++ typing.py
@@ -200,7 +200,7 @@ def admin_allowed(adm_type=AdminType.FULL, ban_enable=True, allowed_types=()):
                     allowed &= check_ban(update, session)
                     if allowed:
                         if func.__name__ not in ['manage_all', 'trigger_show', 'user_panel' 'wrapper', 'welcome']:
-                            log(session, update.effective_user.id, update.effective_chat.id, func.__name__,
+                            (session, update.effective_user.id, update.effective_chat.id, func.__name__,
                                 update.message.text if update.message else None)
                         func(bot, update, session, *args, **kwargs)
             except SQLAlchemyError as err:
