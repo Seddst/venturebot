b53123868c8d5b6695dd6ed0498fc5ae98420c10
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-28 14:11:43 -0700

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -15,7 +15,7 @@ def get_admin_ids(bot, chat_id):
     return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
 
 
-def admin_allowed(func):
+def admin_allowed(func, bot):
     @wraps(func)
     def wrapped(bot, update, *args, **kwargs):
         if update.message.from_user.id in get_admin_ids(bot, update.message.chat_id):
