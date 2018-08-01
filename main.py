1feeebd726c2c15dfc8b1f5bc09e2227e9f4709c
Seddst <39424218+Seddst@users.noreply.github.com>
2018-08-01 14:26:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -424,7 +424,7 @@ def main():
     dp.add_handler(CommandHandler("ban", ban))
     dp.add_handler(CommandHandler("unban", unban))
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
