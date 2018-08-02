8caa0cc4852a3a6f475f11004e13c36400349519
Seddst <39424218+Seddst@users.noreply.github.com>
2018-08-02 12:22:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -148,32 +148,32 @@ def add_trigger_db(msg: Message, chat, trigger_text: str):
     if update.msg.audio:
         trigger.message = msg.audio.file_id
         trigger.message_type = MessageType.AUDIO.value
-    elif telegram.Document:
-        trigger.message = msg.Document.file_id
+    elif telegram.document:
+        trigger.message = msg.document.file_id
         trigger.message_type = MessageType.DOCUMENT.value
-    elif msg.Voice:
-        trigger.message = msg.Voice.file_id
+    elif msg.voice:
+        trigger.message = msg.voice.file_id
         trigger.message_type = MessageType.VOICE.value
-    elif msg.Sticker:
-        trigger.message = msg.Sticker.file_id
+    elif msg.sticker:
+        trigger.message = msg.sticker.file_id
         trigger.message_type = MessageType.STICKER.value
-    elif msg.Contact:
-        trigger.message = str(msg.Contact)
+    elif msg.contact:
+        trigger.message = str(msg.contact)
         trigger.message_type = MessageType.CONTACT.value
-    elif msg.Video:
-        trigger.message = msg.Video.file_id
+    elif msg.video:
+        trigger.message = msg.video.file_id
         trigger.message_type = MessageType.VIDEO.value
     elif msg.video_note:
         trigger.message = msg.video_note.file_id
         trigger.message_type = MessageType.VIDEO_NOTE.value
-    elif msg.Location:
-        trigger.message = str(msg.Location)
+    elif msg.location:
+        trigger.message = str(msg.location)
         trigger.message_type = MessageType.LOCATION.value
-    elif msg.Photo:
-        trigger.message = msg.Photo[-1].file_id
+    elif msg.photo:
+        trigger.message = msg.photo[-1].file_id
         trigger.message_type = MessageType.PHOTO.value
     else:
-        trigger.message = msg.Text
+        trigger.message = msg.text
         trigger.message_type = MessageType.TEXT.value
     Session.add(trigger)
     Session.commit()
